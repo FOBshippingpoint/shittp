9a797e06c389fddb1e2720ca8dddfbc0cf6fbe9d
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2025-10-26 23:42:36 +0800

    naive install

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-# curl -fsSL 
+SHITTP_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.local/config}/shittp"
+
+tmpdir=$(mktemp --directory)
+cd "$tmpdir" || exit 1
+curl -fsSL https://github.com/FOBshippingpoint/shittp/archive/refs/heads/main.zip --output shittp.zip
+unzip shittp.zip
+
+# TODO: specify executable location
+mkdir -p "$HOME/.local/bin"
+mv shittp "$HOME/.local/bin"
+
+if [ -d "$SHITTP_CONFIG_DIR" ]; then
+  echo "[ $SHITTP_CONFIG_DIR ] already exists, ignore"
+else
+  mkdir -p "$SHITTP_CONFIG_DIR"
+  mv -f config "$SHITTP_CONFIG_DIR"
+fi
+
